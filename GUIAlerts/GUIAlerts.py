5544241ad27a40263dcf255667353794730fe39f
Shane <theshanukg@gmail.com>
2019-09-07 22:58:43 -0600

    added secondpage

--- GUIAlerts/GUIAlerts.py
+++ GUIAlerts/GUIAlerts.py
@@ -79,7 +79,7 @@ class Ui_MainWindow(QtWidgets.QWidget):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
  
-        self.pcbut.clicked.connect(self.login)#####
+        self.pcbut.clicked.connect(self.nextwindow)#####
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
@@ -90,7 +90,7 @@ class Ui_MainWindow(QtWidgets.QWidget):
         self.Swibut.setText(_translate("MainWindow", "SWITCH"))
         self.quitbut.setText(_translate("MainWindow", "QUIT"))
     
-    def login(self):
+    def nextwindow(self):
         self.switch_window.emit()
 
 
@@ -98,44 +98,134 @@ class Home(QtWidgets.QWidget):
 
     switch_window = QtCore.pyqtSignal()
 
-    def __init__(self):
-        QtWidgets.QWidget.__init__(self)
-        self.setWindowTitle('Login')
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(800, 600)
+        Dialog.setStyleSheet("background-color: rgb(0, 0, 0);")
+        self.AlertsText = QtWidgets.QTextBrowser(Dialog)
+        self.AlertsText.setGeometry(QtCore.QRect(10, 10, 471, 571))
+        self.AlertsText.setStyleSheet("background-color: rgb(54, 54, 54);\n"
+"color: rgb(255, 255, 255);")
+        self.AlertsText.setObjectName("AlertsText")
+        self.syndicatebut = QtWidgets.QPushButton(Dialog)
+        self.syndicatebut.setGeometry(QtCore.QRect(550, 240, 201, 23))
+        font = QtGui.QFont()
+        font.setFamily("Calibri")
+        font.setPointSize(10)
+        self.syndicatebut.setFont(font)
+        self.syndicatebut.setStyleSheet("background-color: rgb(54, 54, 54);\n"
+"color: rgb(255, 255, 255);")
+        self.syndicatebut.setObjectName("syndicatebut")
+        self.fissurebut = QtWidgets.QPushButton(Dialog)
+        self.fissurebut.setGeometry(QtCore.QRect(550, 270, 201, 23))
+        font = QtGui.QFont()
+        font.setFamily("Calibri")
+        font.setPointSize(10)
+        self.fissurebut.setFont(font)
+        self.fissurebut.setStyleSheet("background-color: rgb(54, 54, 54);\n"
+"color: rgb(255, 255, 255);")
+        self.fissurebut.setObjectName("fissurebut")
+        self.sortiebut = QtWidgets.QPushButton(Dialog)
+        self.sortiebut.setGeometry(QtCore.QRect(550, 300, 201, 23))
+        font = QtGui.QFont()
+        font.setFamily("Calibri")
+        font.setPointSize(10)
+        self.sortiebut.setFont(font)
+        self.sortiebut.setStyleSheet("background-color: rgb(54, 54, 54);\n"
+"color: rgb(255, 255, 255);")
+        self.sortiebut.setObjectName("sortiebut")
+        self.MainPagebg = QtWidgets.QLabel(Dialog)
+        self.MainPagebg.setGeometry(QtCore.QRect(310, 0, 791, 661))
+        self.MainPagebg.setText("")
+        self.MainPagebg.setPixmap(QtGui.QPixmap("Elements/bg.png"))
+        self.MainPagebg.setObjectName("MainPagebg")
+        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton.setGeometry(QtCore.QRect(570, 560, 75, 23))
+        font = QtGui.QFont()
+        font.setFamily("Calibri")
+        self.pushButton.setFont(font)
+        self.pushButton.setStyleSheet("background-color: rgb(54, 54, 54);\n"
+"color: rgb(255, 255, 255);")
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_2.setGeometry(QtCore.QRect(670, 560, 75, 23))
+        font = QtGui.QFont()
+        font.setFamily("Calibri")
+        self.pushButton_2.setFont(font)
+        self.pushButton_2.setStyleSheet("background-color: rgb(54, 54, 54);\n"
+"color: rgb(255, 255, 255);")
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.MainPagebg.raise_()
+        self.AlertsText.raise_()
+        self.syndicatebut.raise_()
+        self.fissurebut.raise_()
+        self.sortiebut.raise_()
+        self.pushButton.raise_()
+        self.pushButton_2.raise_()
 
-        layout = QtWidgets.QGridLayout()
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
 
-        self.button = QtWidgets.QPushButton('Login')
+        self.pushButton.clicked.connect(self.nextwindow)#####
 
-        layout.addWidget(self.button)
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.syndicatebut.setText(_translate("Dialog", "Syndicate Alerts"))
+        self.fissurebut.setText(_translate("Dialog", "Fissure Alerts"))
+        self.sortiebut.setText(_translate("Dialog", "Sortie Alerts"))
+        self.pushButton.setText(_translate("Dialog", "HOME"))
+        self.pushButton_2.setText(_translate("Dialog", "QUIT"))
 
-        self.setLayout(layout)
-
-    def login(self):
+    def nextwindow(self):
         self.switch_window.emit()
+
+
+    #def __init__(self):
+        #QtWidgets.QWidget.__init__(self)
+        #self.setWindowTitle('Login')
+
+        #layout = QtWidgets.QGridLayout()
+
+        #self.button = QtWidgets.QPushButton('Login')
+
+        #layout.addWidget(self.button)
+
+        #self.setLayout(layout)
+
+    #def login(self):
+        #self.switch_window.emit()
 
 
 
 class Controller:
 
-    def __init__(self,ui,window):
+    def __init__(self,ui,ui2,window,dialog):
 
         self.windowone = ui
         self.windowmain = window
 
+        self.windowtwo = ui2
+        self.windowtwomain = dialog
+
     def show_main(self):
 
+        self.windowtwo.close()
         self.windowone.show()
+
         self.windowmain.switch_window.connect(self.show_home)
         self.windowmain.quitbut.clicked.connect(self.windowone.close)
         print(self.windowmain.pcbut.text())
 
     def show_home(self):
 
-        self.home = Home()
-        self.home.switch_window.connect(self.show)
-
         self.windowone.close()
-        self.home.show()
+        self.windowtwo.show()
+
+        #QUIT and HOME button functions, respectively
+        self.windowtwomain.pushButton_2.clicked.connect(self.windowtwo.close)
+        self.windowtwomain.pushButton.clicked.connect(self.show_main)
+
 
     def show_window_two(self, text):
 
@@ -155,11 +245,16 @@ if __name__ == "__main__":
 
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    
+    #main window
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
-    controller = Controller(MainWindow,ui)
+    #home window
+    Dialog = QtWidgets.QDialog()
+    ui2 = Home()
+    ui2.setupUi(Dialog)
+
+    controller = Controller(MainWindow,Dialog,ui,ui2)
     controller.show_main()
     sys.exit(app.exec_())
 
